f053c795cc688363e196d66d9f23e1ed4e37b023
Dhruv Ahuja <83733638+dhruv-ahuja@users.noreply.github.com>
2025-01-19 13:01:35 +0530

    refactor: improve code quality and file naming
    - rename common to schema to better reflect module's purpose
    - format and lint code
    - also update gitignore

--- echo.py
+++ echo.py
@@ -2,17 +2,22 @@
 
 
 from typing import cast
-from common import RequestBody, ResponseBody
-from maelstrom import Node, Request, Body
+
+from maelstrom import Body, Node, Request
+from schema import RequestBody
 
 node = Node()
+
 
 @node.handler
 async def echo(req: Request) -> Body:
     body = req.body
     body = cast(RequestBody, body)
     return {
-        "type": "echo_ok", "in_reply_to": body["msg_id"], "msg_id": body["msg_id"], "echo": body["echo"],
+        "type": "echo_ok",
+        "in_reply_to": body["msg_id"],
+        "msg_id": body["msg_id"],
+        "echo": body["echo"],
     }
 
 
--- schema.py
+++ schema.py
@@ -1,10 +1,12 @@
 from typing import Any, TypedDict
 
+
 class RequestBody(TypedDict):
     msg_id: Any
     in_reply_to: Any | None
     type: str | None
     echo: Any | None
+
 
 class ResponseBody(TypedDict):
     msg_id: Any
